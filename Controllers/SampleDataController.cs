ae7b54d97bf9a47c130265b13d4008ef2043ca96
Domt301 <leonardod.trimarchi@gmail.com>
2017-11-28 13:03:51 -0500

    removed unused using statements

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -9,8 +9,7 @@ namespace netAngularNew.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
-        {
+        private static readonly string[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
@@ -31,14 +30,7 @@ namespace netAngularNew.Controllers
             public string DateFormatted { get; set; }
             public int TemperatureC { get; set; }
             public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
         }
     }
 }
